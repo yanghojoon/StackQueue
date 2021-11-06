f85377cc63420d764089c90b797a9f058d5c63af
yanghojoon <yanghojoon8487@gmail.com>
2021-11-06 22:46:39 +0900

    feat: array를 활용한 Stack과 Queue 구현

--- practiceStackQueue/main.swift
+++ practiceStackQueue/main.swift
@@ -7,5 +7,37 @@
 
 import Foundation
 
-print("Hello, World!")
+// Queue의 데이터 저장
+struct MakeRow {
+    var people: [String] = []
+    
+    mutating func come(name: String) {
+        self.people.append(name)
+    }
+    
+    mutating func goOut(name: String) {
+        self.people.removeFirst()
+    }
+}
+
+// Stack의 데이터 저장
+struct UsingDishes {
+    var dishes: [Int] = []
+    
+    mutating func put(count: Int) {
+        self.dishes.append(count)
+        print("\(count)번째 접시를 놨습니다.")
+    }
+    
+    mutating func pull(count: Int) {
+        self.dishes.removeLast()
+        print("\(count)번째 접시를 꺼냈습니다.")
+    }
+}
+
+var employee = MakeRow()
+employee.come(name: "호댕")
+employee.come(name: "요한")
+employee.come(name: "스티븐")
+
 
